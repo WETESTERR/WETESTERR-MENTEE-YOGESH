194dc536905d142dbef3962917a677663002bb0c
Yogesh More <65850665+moreyogesh19@users.noreply.github.com>
2020-06-18 22:50:18 +0530

    Java Learnings

---
+++ Java_coding/src/javaPractice/SecondClass.java
@@ -0,0 +1,15 @@
+package javaPractice;
+
+public class SecondClass {
+	
+	public void SecondMethod()
+	{
+		System.out.println("I am from secondMethod of SecondClass");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
