017cdd45132161112f2f75eaac9c14d2c0341996
Yogesh More <65850665+moreyogesh19@users.noreply.github.com>
2020-08-16 20:36:37 +0530

    added cucumber-zerobank practice

---
+++ cucumber/zerobank/src/main/java/org/zerobank/App.java
@@ -0,0 +1,13 @@
+package org.zerobank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
